c23f41bffd673405155b7c36bdea57f338214673
cVitorAndrade <vitorandrade1804@gmail.com>
2026-04-20 23:41:18 +0000

    fix(database): remove PrismaClient instantiation to support Prisma 7

--- packages/database/prisma/migrations/20260420221359_init_database/migration.sql
+++ packages/database/prisma/migrations/20260420221359_init_database/migration.sql
@@ -1,20 +1,21 @@
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "avatarUrl" TEXT,
     "role" TEXT NOT NULL,
     "status" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
